cbf5c301190901cf34e9a991dc4739a4f13bcdef
jozef mantis <wej.bakker@mantisbv.nl>
2023-02-05 21:22:34 +0100

    v5-2-21

--- ploegenlijst/settings.py
+++ ploegenlijst/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-)r2waboda$o)g39!ap!l7dx$numws6k7zi9=m*3e1hbudc!2&r'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*', 'http://127.0.0.1:8000',
 'https://kluisjeslijst.up.railway.app',]
 
