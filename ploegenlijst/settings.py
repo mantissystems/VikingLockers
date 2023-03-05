45bc5653eddda4de5dd460f4af3c6c89aea523a1
jozef mantis <wej.bakker@mantisbv.nl>
2023-03-05 20:55:52 +0100

    v5-3-20

--- ploegenlijst/settings.py
+++ ploegenlijst/settings.py
@@ -66,11 +66,22 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ploegenlijst.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'viking.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'viking.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'railway',
+       'USER': 'postgres',
+       'PASSWORD':'BxRi0NiNO785IT2ClrRr',
+       'HOST':'containers-us-west-181.railway.app',
+       'PORT': '5654'
+   }
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
