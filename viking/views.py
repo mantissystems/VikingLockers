d1c946f9a28a1e5f580a6a43475d663dfe729d0c
jozef mantis <wej.bakker@mantisbv.nl>
2023-04-27 16:58:45 +0200

    v27-4-17

--- viking/views.py
+++ viking/views.py
@@ -3,9 +3,9 @@ from django.template import loader
 import csv
 from django.http import HttpResponse
 # from django.http import StreamingHttpResponse
-import reportlab
-from django.http import FileResponse
-from reportlab.pdfgen import canvas
+# import reportlab
+# from django.http import FileResponse
+# from reportlab.pdfgen import canvas
 import io
 from urllib import request
 from django.shortcuts import render
@@ -933,26 +933,26 @@ def file_load_view(request):
 #         """Write the value by returning it, instead of storing in a buffer."""
 #         return value
 
-def some_view(request):
-    # import reportlab
-    # from django.http import FileResponse
-    # from reportlab.pdfgen import canvas
+# def some_view(request):
+#     # import reportlab
+#     # from django.http import FileResponse
+#     # from reportlab.pdfgen import canvas
 
-    # Create a file-like buffer to receive PDF data.
-    buffer = io.BytesIO()
+#     # Create a file-like buffer to receive PDF data.
+#     buffer = io.BytesIO()
 
-    # Create the PDF object, using the buffer as its "file."
-    p = canvas.Canvas(buffer)
+#     # Create the PDF object, using the buffer as its "file."
+#     p = canvas.Canvas(buffer)
 
-    # Draw things on the PDF. Here's where the PDF generation happens.
-    # See the ReportLab documentation for the full list of functionality.
-    p.drawString(10, 10, "Hello world.")
+#     # Draw things on the PDF. Here's where the PDF generation happens.
+#     # See the ReportLab documentation for the full list of functionality.
+#     p.drawString(10, 10, "Hello world.")
 
-    # Close the PDF object cleanly, and we're done.
-    p.showPage()
-    p.save()
+#     # Close the PDF object cleanly, and we're done.
+#     p.showPage()
+#     p.save()
 
-    # FileResponse sets the Content-Disposition header so that browsers
-    # present the option to save the file.
-    buffer.seek(0)
-    return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
+#     # FileResponse sets the Content-Disposition header so that browsers
+#     # present the option to save the file.
+#     buffer.seek(0)
+#     return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
