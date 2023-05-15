af3fd1f6b39d552038b76333fed32be1b136d69e
jozef mantis <wej.bakker@mantisbv.nl>
2023-05-15 16:03:53 +0200

    v15-5-16

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Room, Topic, Message,KluisjesRV
+from base.models import User,AbstractUser
+admin.site.register(User)
+admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
+admin.site.register(KluisjesRV)
