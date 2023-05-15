fc18000001c65923af2c1fdc07debe1eb0c9d595
jozef mantis <wej.bakker@mantisbv.nl>
2023-05-15 15:28:42 +0200

    v15-5-15

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from import_export.admin import ImportExportModelAdmin
+from .models import Room, Topic, Message,File,KluisjesRV
+@admin.register(KluisjesRV)
+class FileAdmin(ImportExportModelAdmin):
+    pass
