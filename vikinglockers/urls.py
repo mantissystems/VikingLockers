3661456bc67a30aadfc5683cefc8071e69d66c95
jozef mantis <wej.bakker@mantisbv.nl>
2023-05-24 10:16:40 +0200

    v24-5-10

--- vikinglockers/urls.py
+++ vikinglockers/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', include('base.urls')),
     # path('__debug__/', include('debug_toolbar.urls')),    
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
